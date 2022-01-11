8f3fcaec4c61c6776cad81dcc59dcce0a9522466
Junjun Zhang <topcheer@me.com>
2022-01-11 17:33:51 +0800

    bump version

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ import (
 	"time"
 )
 
-var Version = "v1.1.20"
+var Version = "v1.2.0"
 
 type FsHost struct {
 	mfs *fuse.MountedFileSystem
--- main_windows.go
+++ main_windows.go
@@ -20,7 +20,7 @@ import (
 	"time"
 )
 
-var Version = "v1.1.20"
+var Version = "v1.2.0"
 
 type FsHost struct {
 	//host *fuse.FileSystemHost
